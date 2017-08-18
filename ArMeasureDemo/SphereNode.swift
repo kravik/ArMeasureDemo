e17d204066b5cd397aa38e9b82bd732a2da21e71
Igor Kravtsov <igor.kravtsov@qbeats.com>
2017-08-18 10:41:03 -0400

    Initial commit

---
+++ ArMeasureDemo/SphereNode.swift
@@ -0,0 +1,26 @@
+//
+//  SphereNode.swift
+//  ArMeasureDemo
+//
+//  Created by Igor K on 8/17/17.
+//  Copyright © 2017 Igor K. All rights reserved.
+//
+
+import SceneKit
+
+class SphereNode: SCNNode {
+    init(position: SCNVector3) {
+        super.init()
+        let sphereGeometry = SCNSphere(radius: 0.005)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        material.lightingModel = .physicallyBased
+        sphereGeometry.materials = [material]
+        self.geometry = sphereGeometry
+        self.position = position
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- ArMeasureDemo/ViewController.swift
+++ ArMeasureDemo/ViewController.swift
@@ -7,19 +7,92 @@
 //
 
 import UIKit
+import ARKit
+import SceneKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
+    var nodes: [SphereNode] = []
+
+    lazy var sceneView: ARSCNView = {
+        let view = ARSCNView(frame: CGRect.zero)
+        view.delegate = self
+        view.autoenablesDefaultLighting = true
+        return view
+    }()
+    
+    lazy var infoLabel: UILabel = {
+        let label = UILabel(frame: CGRect.zero)
+        label.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.title1)
+        label.textAlignment = .center
+        label.backgroundColor = .white
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(sceneView)
+        view.addSubview(infoLabel)
+        
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        tapRecognizer.numberOfTapsRequired = 1
+        sceneView.addGestureRecognizer(tapRecognizer)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        sceneView.frame = view.bounds
+        infoLabel.frame = CGRect(x: 0, y: 16, width: view.bounds.width, height: 64)
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.isLightEstimationEnabled = true
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    // MARK: ARSCNViewDelegate
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+        var status = "Loading..."
+        switch camera.trackingState {
+        case ARCamera.TrackingState.notAvailable:
+            status = "Not available"
+        case ARCamera.TrackingState.limited(_):
+            status = "Analyzing..."
+        case ARCamera.TrackingState.normal:
+            status = "Ready"
+        }
+        infoLabel.text = status
+    }
+    
+    // MARK: Gesture handlers
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        let tapLocation = sender.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation, types: .featurePoint)
+        if let result = hitTestResults.first {
+            let position = SCNVector3.positionFrom(matrix: result.worldTransform)
+            let sphere = SphereNode(position: position)
+            sceneView.scene.rootNode.addChildNode(sphere)
+            let lastNode = nodes.last
+            nodes.append(sphere)
+            if lastNode != nil {
+                let distance = lastNode!.position.distance(to: sphere.position)
+                infoLabel.text = String(format: "Distance: %.2f meters", distance)
+            }
+        }
+    }
 }
 
+extension SCNVector3 {
+    func distance(to destination: SCNVector3) -> CGFloat {
+        let dx = destination.x - x
+        let dy = destination.y - y
+        let dz = destination.z - z
+        return CGFloat(sqrt(dx*dx + dy*dy + dz*dz))
+    }
+    
+    static func positionFrom(matrix: matrix_float4x4) -> SCNVector3 {
+        let column = matrix.columns.3
+        return SCNVector3(column.x, column.y, column.z)
+    }
+}
